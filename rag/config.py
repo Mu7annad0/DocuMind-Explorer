acdb859b41f878378a44655f41ac6347cbe2ac25
Muhannad Ashraf <mu7nnd.as@gmail.com>
2024-09-07 01:38:47 +0300

    Update config.py

---
+++ rag/config.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+
+class Config:
+    class Path:
+        APP_HOME = Path(os.getenv("APP_HOME", Path(__file__).parent.parent))
+        DATABASE_DIR = APP_HOME / "docs-db"
+        DOCUMENTS_DIR = APP_HOME / "tmp"
+        IMAGES_DIR = APP_HOME / "images"
+
+    class Database:
+        DOCUMENT_COLLECTION = "documents"
+
+    class Model:
+        EMBEDDING_MODEL = "BAAI/bge-base-en-v1.5"
+        RERANKER = "ms-marco-MINILM-L-12-v2"
+        LOCAL_LLM = "gemma2:9b"
+        TEMP = 0.0
+        MAX_TOKENS = 7000
+        USE_LOCAL_LLM = True
+
+    class Retriever:
+        USE_RERANKER = True
+        USE_CHAIN_FILTER = False
+
+    DEBUG = True
+    CONV_MESSAGES = 6
