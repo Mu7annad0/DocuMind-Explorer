84ada9ff19a6befdc41fabfed310c7c5e33c6d28
Muhannad Ashraf <mu7nnd.as@gmail.com>
2025-01-21 06:05:32 +0200

    refactor: Streamlit app.

--- app.py
+++ app.py
@@ -1,145 +1,114 @@
-# Import necessary libraries
+"""Streamlit App."""
 import asyncio
+import random
+import nest_asyncio
 import streamlit as st
 from dotenv import load_dotenv
-from langchain_community.chat_models import ChatOllama
-
-# Import custom modules for RAG functionality
-from rag.chain import ask_question, create_chain
+from rag.chain import ask_question, build_qa_chain
 from rag.config import Config
-from rag.ingestor import Ingestor
-from rag.retriever import create_retriever
-from rag.uploader import upload_file
-
-# Load environment variables
 load_dotenv()
+nest_asyncio.apply()
 
-# Initialize the language model
-llm = ChatOllama(
-            model=Config.Model.LOCAL_LLM,
-            temperature=Config.Model.TEMP,
-            keep_alive="1h",
-            max_tokens=Config.Model.MAX_TOKENS,
-        )
-
-# Cache the QA chain building function
 @st.cache_resource(show_spinner=False)
-def build_qa_chain(files):
-    # Upload files and get their paths
-    file_paths = upload_file(files)
-    # Ingest documents into a vector database
-    vector_db = Ingestor().ingest(doc_files=file_paths)
-    # Create a retriever for the vector database
-    retriever = create_retriever(llm, vector_db)
-    # Create and return the QA chain
-    return create_chain(llm, retriever)
- 
-# Asynchronous function to ask questions to the chain
+def build_chain(files):
+    """return the qa Chain"""
+    return build_qa_chain(files)
+
+LOADING_MESSAGES = [
+    "stand by!"
+]
+
 async def ask_chain(question: str, chain):
+    """Asking"""
     full_response = ""
-    async for event in ask_question(chain, question, session_id="session-id-42"):
-        if isinstance(event, str):
-            # Accumulate string responses
-            full_response += event
-            yield full_response
-        elif isinstance(event, list):
-            # Yield list responses (likely source documents)
-            yield event
+    assistant = st.chat_message("assistant")
+    with assistant:
+        message_placeholder = st.empty()
+        message_placeholder.status(random.choice(LOADING_MESSAGES), state="running")
+        documents = []
+        async for event in ask_question(chain, question, session_id="session-id-42"):
+            if type(event) is str:
+                full_response += event
+                message_placeholder.markdown(full_response)
+            if type(event) is list:
+                documents.extend(event)
+        for i, doc in enumerate(documents):
+            with st.expander(f"Source #{i+1}"):
+                st.write(doc.page_content)
 
-# Function to handle document upload in the sidebar
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+
 def show_upload_documents():
-    st.sidebar.title("🚀 DocuMind Explorer")
-    # st.sidebar.subheader("Unleash Your Documents")
-    # File uploader for PDF documents
-    uploaded_files = st.sidebar.file_uploader(
-        label="Upload PDF files to fuel the AI", type=["pdf"], accept_multiple_files=True
-    )
+    """Show the uploaded docs"""
+    holder = st.empty()
+    with holder.container():
+        st.header("RagBase")
+        st.subheader("Get answers from your documents")
+        uploaded_files = st.file_uploader(
+            label="Upload PDF files", type=["pdf"], accept_multiple_files=True
+        )
     if not uploaded_files:
-        # Display a warning if no files are uploaded
-        st.sidebar.warning("Feed me PDFs, and I shall grant you wisdom! 📚✨")
-        return None
-    # Build and return the QA chain if files are uploaded
-    with st.spinner("Processing your documents..."):
-        return build_qa_chain(uploaded_files)
+        st.warning("Please upload PDF documents to continue!")
+        st.stop()
 
-# Function to display the chat interface
-def show_chat_interface(chain):
-    st.title("Chat with Your Documents")
-    
-    # Initialize chat history if it doesn't exist
-    if "messages" not in st.session_state:
-        st.session_state.messages = [
-            {
-                "role": "assistant",
-                "content": "Greetings, knowledge seeker! What mysteries shall we unravel from your documents today?",
-            }
-        ]
+    with st.spinner("Analyzing your document(s)..."):
+        holder.empty()
+        return build_chain(uploaded_files)
 
-    # Display chat history
+
+def show_message_history():
+    """Show the message history"""
     for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
+        role = message["role"]
+        with st.chat_message(role):
             st.markdown(message["content"])
 
-    # Handle user input
-    if prompt := st.chat_input("Pose your query to the all-knowing AI..."):
-        # Add user message to chat history
+
+def show_chat_input(chain):
+    """Show the chat input for the user"""
+    if prompt := st.chat_input("Ask your question here"):
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        # Generate and display AI response
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            message_placeholder.markdown("Give me a moment, I'm processing............")
-            full_response = ""
-            async def process_response():
-                nonlocal full_response
-                async for response in ask_chain(prompt, chain):
-                    if isinstance(response, str):
-                        # Update response in real-time
-                        full_response = response
-                        message_placeholder.markdown(full_response + "▌")
-                    elif isinstance(response, list):
-                        # Display relevant source documents
-                        st.write("📜 Relevant Scrolls of Knowledge:")
-                        for i, doc in enumerate(response):
-                            with st.expander(f"Tome {i+1}"):
-                                st.write(doc.page_content)
-                return full_response
+        # Run the async function in the event loop
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(ask_chain(prompt, chain))
 
-            # Run the asynchronous response processing
-            full_response = asyncio.run(process_response())
-            message_placeholder.markdown(full_response)
-        # Add AI response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
-    
-    # Check if conversation limit is reached
-    if Config.CONV_MESSAGES > 0 and Config.CONV_MESSAGES <= len(st.session_state.messages):
-        st.warning(
-            "You've reached the limits of this conversation. Refresh to embark on a new quest for knowledge!"
-        )
 
 # Set up the Streamlit page configuration
-st.set_page_config(page_title="DocuMind Explorer", layout="wide", page_icon="🚀")
+st.set_page_config(page_title="RagBase", page_icon="🐧")
 
-# Customize the app's appearance
-# st.markdown(
-#     """
-#     <style>
-#     .stApp {
-#         background-color: #3C3D37;  /* Dark blue-gray color for main background */
-#     }
-#     [data-testid="stSidebar"] {
-#         background-color: #181C14;  /* Slightly lighter blue-gray for sidebar */
-#     }
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-# )
+# Custom CSS for styling
+st.html(
+    """
+<style>
+    .st-emotion-cache-p4micv {
+        width: 2.75rem;
+        height: 2.75rem;
+    }
+</style>
+"""
+)
+
+# Initialize session state for messages
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Hi! What do you want to know about your documents?",
+        }
+    ]
+
+# Check conversation limit
+if Config.CONV_MESSAGES > 0 and Config.CONV_MESSAGES <= len(st.session_state.messages):
+    st.warning(
+        "You have reached the conversation limit. Refresh the page to start a new conversation."
+    )
+    st.stop()
 
 # Main app logic
-chain = show_upload_documents()
-if chain:
-    show_chat_interface(chain)
-else:
-    st.info("Upload your sacred texts in the sidebar to begin our journey of discovery! 📚🔍")
+ch = show_upload_documents()
+show_message_history()
+show_chat_input(ch)
